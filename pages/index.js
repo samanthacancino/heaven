f172b732fa126803067359521c0a066b23d8dfca
Samantha Cancino <samanthacancino@gmail.com>
2022-02-23 21:12:22 +0100

    Some repositories connected and running

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,9 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-export default function Home( pinnedItems ) {
-  console.log(pinnedItems);
+export default function Home({ pinnedItems }) {
+  console.log('pinnedItems', pinnedItems);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,42 +22,18 @@ export default function Home( pinnedItems ) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          Some repositories by{' '}
+          <a href='https://github.com/samanthacancino/'>samanthacancino</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/canary/examples'
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {pinnedItems.map((item) => {
+            return (
+              <a key={item.id} href={item.url} className={styles.card}>
+                <h2>{item.name}</h2>
+              </a>
+            );
+          })}
         </div>
       </main>
 
@@ -117,9 +94,9 @@ export async function getStaticProps() {
       }
     `,
   });
-  const { user } = data
-  const { pinnedItems } = user.pinnedItems.edges.map(edge => edge.node)
 
+const { user } = data;
+const pinnedItems = user.pinnedItems.edges.map((edge) => edge.node);
   return {
     props: {
       pinnedItems
